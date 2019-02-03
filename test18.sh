c556bbc213bb7ad74eed5d7534391068b72bcac9
root <root@localhost.com>
2019-02-03 22:10:00 +0800

    201902032210 commit

--- test18.sh
+++ test18.sh
@@ -3,12 +3,15 @@ change_file()
 {    
   dir=/root/practice/sh
   date=`date +'%Y%m%d%H%M'`
-  find $dir/ -type f -mmin -3600>/tmp/file.list
+  find $dir/ -type f -mmin -30>/tmp/file.list
   n1=`wc -l /tmp/file.list | awk '{print $1}'`
   if [ $n1 -ne 0 ];then
     git add $dir/*.sh
-    git commit -m "$date"
+    git commit -m "$date commit"
     git push origin master
+    if [ $? == 0];then
+      echo "git push successful." >>/tmp/$date.log
+    fi
     echo "`date +"%D %H:%M:%S"`" >>/tmp/$date.log
     echo '===============================' >>/tmp/$date.log
     cat /tmp/file.list >> /tmp/$date.log
@@ -19,7 +22,7 @@ change_file()
 while true
   do 
     mail_date=`date +%H:%M:%S`
-    if [ "$mail_date" == "12:00:00" ];then
+    if [ "$mail_date" == "22:10:00" ];then
       change_file
       sn=`wc -l /tmp/file.list | awk '{print $1}'`
       /usr/bin/mailx -s " Yesterday you Practiced $sn scirpts, Come On!" 568867090@qq.com < /tmp/$date.log
--- test20.sh
+++ test20.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-
+This is a testflie for git push.
