40854f6077df909728cf6ff27efd3318db2fc6ff
root <root@localhost.com>
2019-02-03 22:50:00 +0800

    2019-02-03 22:50:00 commit

--- test18.sh
+++ test18.sh
@@ -3,7 +3,7 @@ change_file()
 {    
   dir=/root/practice/sh
   date=`date +'%Y%m%d%H%M'`
-  find $dir/ -type f -mmin -3600>/tmp/file.list
+  find $dir/ -type f -mmin -1440>/tmp/file.list
   n1=`wc -l /tmp/file.list | awk '{print $1}'`
   if [ $n1 -ne 0 ];then
     git add $dir/*.sh
@@ -22,7 +22,7 @@ change_file()
 while true
   do 
     mail_date=`date +%H:%M:%S`
-    if [ "$mail_date" == "22:35:00" ];then
+    if [ "$mail_date" == "22:50:00" ];then
       change_file
       sn=`grep '.sh$' /tmp/file.list | wc -l | awk '{print $1}'`
       /usr/bin/mailx -s " Yesterday you Practiced $sn scirpts, Come On!" 568867090@qq.com < /tmp/$date.log
