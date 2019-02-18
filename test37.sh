121df3186c9a3b8f444fe772c380adc349e22f45
root <root@localhost.com>
2019-02-18 23:03:00 +0800

    2019-02-18 23:03:00 commit

--- test37.sh
+++ test37.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 for n in `seq 1 254`
 do
-  ipadr=192.168.0.$n
+  ipadr=192.168.253.$n
   n=`ping -c 1 $ipadr | grep packet | awk -F', ' '{print $3}' | awk -F'%' '{print $1}'`
   if [ $n == 0 ];then
     echo "$ipadr is up."
